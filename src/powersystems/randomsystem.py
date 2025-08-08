32410ccc4763b50a25f6d004ab16697c7741878b
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-08-08 17:21:27 +0200

    add random system

---
+++ src/powersystems/randomsystem.py
@@ -0,0 +1,79 @@
+from tensorpowerflow import GridTensor
+import numpy as np
+from time import perf_counter
+import torch
+
+class randomsystem:
+    def __init__(self, 
+                 active_power: np.ndarray = None,
+                 reactive_power: np.ndarray = None,
+                 num_nodes: int = 100,
+                 num_children: int = 3,
+                 ):
+        """
+        Initialize the randomsystem class with optional reactive power input.
+        
+        Parameters:
+        reactive_power (np.ndarray): Optional array of reactive power values.
+        active_power (np.ndarray): Optional array of active power values.
+        """
+        self.active_power = active_power
+        self.reactive_power = reactive_power
+        self.num_nodes = num_nodes
+        self.num_children = num_children
+        self.gpu = self._checkgpu()
+        
+    def _checkgpu(self):
+        """
+        Check if a GPU is available for computation.
+        
+        Returns:
+        bool: True if a GPU is available, False otherwise.
+        """
+        return torch.cuda.is_available()
+
+    def run(self,
+            random_seed: int = 42,
+            plot_graph: bool = True
+            ):
+        """
+        Run the power flow analysis for the random system.
+        
+        Returns:
+        dict: A dictionary containing the voltage magnitudes at each node.
+        
+        dict_keys(['v', 'time_pre_pf', 'time_pf', 'time_algorithm', 'iterations',
+        'convergence', 'iterations_log', 'time_pre_pf_log', 'time_pf_log', 'convergence_log'])
+        """
+        # Check if there is a GPU available and set the device accordingly
+        device = self.gpu
+        
+        # Generate a system
+        network_rnd = GridTensor.generate_from_graph(
+            nodes=self.num_nodes,
+            child=self.num_children,
+            plot_graph=plot_graph,
+            gpu_mode=device,
+        )
+
+        # Run power flow analysis
+        solution = network_rnd.run_pf(
+            active_power=self.active_power,
+            reactive_power=self.reactive_power
+        )
+        
+        return solution
+    
+    
+if __name__ == "__main__":
+    # Example usage of the randomsystem class
+    active_power = np.random.normal(50, scale=1, size=(100, 99))  # Power in kW
+    reactive_power = np.random.normal(10, scale=0.5, size=(100, 99))  # Reactive power in kVAR
+    
+    system = randomsystem(active_power=active_power, reactive_power=reactive_power, num_nodes=100, num_children=4)
+    result = system.run()
+    
+    print("Voltage magnitudes at each node:", len(result))
+    print("Voltage magnitudes:", result["v"].shape)
+    print("Convergence status:", result.keys())
+    
