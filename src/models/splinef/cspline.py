582428349f845c36e17a66cb61845b7486b81286
Weijie Xia <weijiexia@tudelft.nl>
2025-09-01 15:44:43 +0200

    finish realnvp

---
+++ src/models/splinef/cspline.py
@@ -0,0 +1,10 @@
+import os
+import sys
+_parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+print(_parent_dir)
+sys.path.append(_parent_dir)
+
+import torch
+import src.models.basicnetwork.basicnets as basicnets
+
+class CRealnvpBasic(torch.nn.Module):
