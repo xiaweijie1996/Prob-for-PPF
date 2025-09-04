8ffd2c637c5ce1a93ae7631a595b92e1a5fb0983
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-09-04 10:27:11 +0200

    add task sh

---
+++ src/training/crealnvp/trainrealnvp34node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name="RealNVP34"
+#SBATCH --partition=gpu-a100
+#SBATCH --time=99:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=research-eemcs-ese
+
+module load 2023r1 
+module load cuda/11.6
+
+srun src/training/crealnvp/main_34.py
