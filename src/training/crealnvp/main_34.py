b92b5f47353855f5137095663eb0a3a8735dccf5
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-09-04 22:36:29 +0200

    add

--- src/training/crealnvp/main_34.py
+++ src/training/crealnvp/main_34.py
@@ -190,7 +190,7 @@ def main():
         })
         
         # Save the model every 100 epochs
-        if (_ + 1) >100 and end_loss > loss_forward.item():
+        if (_ + 1) > 10000 and end_loss > loss_forward.item():
             end_loss = loss_forward.item()
             torch.save(realnvp_model.state_dict(), os.path.join(save_path, f"CRealnvpmodel_{num_nodes}.pth"))
             print(f"saved at epoch {_+1} with loss {end_loss}")
