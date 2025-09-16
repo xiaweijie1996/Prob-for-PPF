2141275e2d17b9cc32e0b8bd2fb4bac829331538
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-09-16 16:03:37 +0200

    add

--- src/powersystems/ieee39.py
+++ src/powersystems/ieee39.py
@@ -41,6 +41,25 @@ class Case39PF:
         self.net.load["p_mw"] = p_vec
         self.net.load["q_mvar"] = q_vec
         
+    def pf_input(self):
+        """
+        Three types of the bus:
+        - PQ bus: input P, Q; output V, θ
+        - PV bus: input P, V; output Q, θ
+        - Slack bus: input V, θ; output P, Q
+        
+        return the input of the power flow analysis
+        """
+        input_dic = {}
+        input_dic['bus_p'] = self.net.load.p_mw
+        input_dic['bus_q'] = self.net.load.q_mvar
+        input_dic["v_bus"] = self.net.ext_grid.vm_pu
+        
+        # input_dic['pv_buses'] = self.net.sgen.bus.values.tolist()  # PV buses
+        # input_dic['slack_buses'] = self.net.ext_grid.bus.values.tolist()  # Slack bus
+        return input_dic
+        
+        
     def run_pf(self, max_iteration="auto"):
         """
         Runs the power flow analysis on the network.
@@ -57,15 +76,22 @@ class Case39PF:
 if __name__ == "__main__":
 
     case39 = Case39PF()
-    case39._diagnose()
-    
+    case39._diagnose
+    print(case39.net.res_bus)
     # change the loads
     length = len(case39.net.load)
-    p_vec = [199 * 1 for i in range(length)]  # Example active power values
-    q_vec = [30 * 1 for i in range(length)]
+    p_vec = [199 * 1 + np.random.randn() * 1 for i in range(length)]
+    q_vec = [30 * 1 + np.random.randn() * 1 for i in range(length)]
     case39.set_loads(p_vec, q_vec)
+    
+    # Sys input
+    # input_dic = case39.pf_input()
+    # print("Power flow input:", input_dic)
     
     # Example usage of the power flow analysis
     print("Running power flow analysis with modified loads...")
     result = case39.run_pf()
     print("Power flow results:", result)
+    
+    # Plot
+    pp.plotting.plotly.simple_plotly(case39.net, filename="src/powersystems/ieee39_network.html")
