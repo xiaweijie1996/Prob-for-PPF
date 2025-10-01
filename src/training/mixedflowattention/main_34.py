0c1cc059ad495a5bb46aefad29fb6e339eca9351
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-10-01 16:29:35 +0200

    change ssave time

--- src/training/mixedflowattention/main_34.py
+++ src/training/mixedflowattention/main_34.py
@@ -207,7 +207,7 @@ def main():
         })
         
         # Save the model every 100 epochs
-        if (_ + 1) > 1 and end_loss > loss_vtp.item():
+        if (_ + 1) > 100000 and end_loss > loss_vtp.item():
             end_loss = loss_vtp.item()
             torch.save(mix_model.state_dict(), os.path.join(save_path, f"MixedAttentionmodel_{num_nodes}.pth"))
             print(f"saved at epoch {_+1} with loss {end_loss}")
