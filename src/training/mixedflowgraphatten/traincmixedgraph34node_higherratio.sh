85394a6156143eabad011e9a8ae7b0ae40bcba34
xiaweijie1996 <xiaweijie1996@gmail.com>
2025-10-03 12:23:36 +0200

    add

--- src/training/mixedflowgraphatten/traincmixedgraph34node_higherratio.sh
+++ src/training/mixedflowgraphatten/traincmixedgraph34node_higherratio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J GrapCSMixedhAttention
+#SBATCH -J HRGrapCSMixedhAttention
 #SBATCH -p gpu_a100           # use a GPU partition that 'sinfo' shows for your account
 #SBATCH -t 99:00:00
 #SBATCH -N 1
